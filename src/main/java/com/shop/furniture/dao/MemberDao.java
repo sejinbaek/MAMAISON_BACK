a420c4db6b66732c4650ffe6ac81047eee447940
sejin <sejin@DESKTOP-ICG4EP2>
2025-03-09 21:12:07 +0900

    refactor: MyBatis 적용

--- src/main/java/com/shop/furniture/dao/MemberDao.java
+++ src/main/java/com/shop/furniture/dao/MemberDao.java
@@ -7,66 +7,17 @@ import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.ibatis.annotations.Mapper;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Repository;
 
 import com.shop.furniture.dto.Member;
 import com.shop.furniture.dto.Product;
 
-@Repository
-public class MemberDao {
+@Mapper
+public interface MemberDao {
+	public void insertMember(Member m) throws Exception;
 	
-	@Value("${spring.datasource.driver-class-name}")
-	private String DB_DRIVER;
-	
-	@Value("${spring.datasource.url}")
-	private String DB_URL;
-	
-	@Value("${spring.datasource.username}")
-	private String DB_USER;
-	
-	@Value("${spring.datasource.password}")
-	private String DB_PW;
-
-	
-	public void insertMember(Member m) throws Exception {
-		Class.forName(DB_DRIVER);
-		try(
-			Connection con = DriverManager.getConnection(DB_URL, DB_USER, DB_PW);
-			PreparedStatement stmt = con.prepareStatement("insert into member(name, email, id, password, passwordcheck) values(?,?,?,?,?)");
-				) {
-			stmt.setString(1, m.getName());
-			stmt.setString(2, m.getEmail());
-			stmt.setString(3, m.getId());
-			stmt.setString(4, m.getPassword());
-			stmt.setString(5, m.getPasswordcheck());
-			
-			int i = stmt.executeUpdate();
-			
-			System.out.println(i + "행이 insert 되었습니다");	
-			
-		}
-	}
-	
-	public Member login(Member m) throws Exception {
-		Class.forName(DB_DRIVER);
-		try(
-			Connection con = DriverManager.getConnection(DB_URL, DB_USER, DB_PW);
-			PreparedStatement stmt = con.prepareStatement("select * from member where id=? and password=?");
-				) {
-			
-			stmt.setString(1, m.getId());
-			stmt.setString(2, m.getPassword());
-			
-			ResultSet rs = stmt.executeQuery();
-			
-			if(rs.next()) {
-				String name = rs.getString("name");
-				m.setName(name);
-			}
-			return m;
-			
-		}
-	}
+	public Member login(Member m) throws Exception;
 
 }
--- src/main/java/com/shop/furniture/dao/ProductDao.java
+++ src/main/java/com/shop/furniture/dao/ProductDao.java
@@ -6,48 +6,15 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.*;
 
+import org.apache.ibatis.annotations.Mapper;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Repository;
 
 import com.shop.furniture.dto.Product;
 
-@Repository
-public class ProductDao {
+@Mapper
+public interface ProductDao {
 	
-	@Value("${spring.datasource.driver-class-name}")
-	private String DB_DRIVER;
+	public List<Product> getOnepageProducts() throws Exception;
 	
-	@Value("${spring.datasource.url}")
-	private String DB_URL;
-	
-	@Value("${spring.datasource.username}")
-	private String DB_USER;
-	
-	@Value("${spring.datasource.password}")
-	private String DB_PW;
-
-	
-	public List<Product> getOnepageProducts() throws Exception {
-		Class.forName(DB_DRIVER);
-		try(
-				Connection con = DriverManager.getConnection(DB_URL, DB_USER, DB_PW);
-				PreparedStatement stmt = con.prepareStatement("select * from product limit 8");
-				ResultSet rs = stmt.executeQuery();
-				) {
-			
-			List<Product> list = new ArrayList<>();
-			while(rs.next()) {
-				int prodcode = rs.getInt("prodcode");
-				String prodname = rs.getString("prodname");
-				int price = rs.getInt("price");
-				String pimg = rs.getString("pimg");
-				list.add(new Product(prodname, pimg, prodcode, price));
-			}
-			
-			return list;
-			
-		}
-		
-	}
-
 }
